b15d91c1c3441f6792f503025c4a65cb1caf1f1b
2422439086 <129026042+2422439086@users.noreply.github.com>
2023-07-28 09:47:54 +0800

    week2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import { Button,Drawer } from 'antd';
+import { useState,useEffect} from 'react';
+import { Button, Drawer } from 'antd';
 import { Input } from 'antd';
 import { Layout, Space } from 'antd';
 import { Image } from 'antd';
 import { Typography } from 'antd';
 import { Col, Divider, Row } from 'antd';
 import { Rate } from 'antd';
+import Sider from 'antd/es/layout/Sider';
+import Title from 'antd/es/typography/Title';
 
 const { TextArea } = Input;
 
-const { Title } = Typography;
 
 //layout style
-const { Header,Content } = Layout;
-const headerStyle= {
+const { Header, Content } = Layout;
+const headerStyle = {
   textAlign: 'center',
   color: 'white',
   height: 64,
@@ -26,202 +27,136 @@ const headerStyle= {
 const contentStyle = {
   textAlign: 'left',
   minHeight: 50,
-  padding:'20px 20px',
+  padding: '20px 20px',
   color: 'black',
   backgroundColor: 'white',
 };
 
 //button style
-const ButtonStyle={
-  textAlign:'right',
-  backgroundColor:'red',
-  color:'white',
+const ButtonStyle = {
+  textAlign: 'right',
+  backgroundColor: 'red',
+  color: 'white',
 }
-const ButtonCloseStyle={
-  backgroundColor:'grey',
-  color:'white',
+const ButtonCloseStyle = {
+  backgroundColor: 'grey',
+  color: 'white',
+}
+
+const dataSource=[
+  {
+    id: 1,
+    image: 'https://preview.qiantucdn.com/58pic/40/86/03/88P58PICt6vC6E6DQZp46_PIC2018.jpg%21w1024_new_small',
+    title: '肥牛米粉（大份） 1份',
+    description: '柳州螺狮粉',
+    price: 25,
+    commemt: 'comment',
+    rate: 4,
+  },
+  {
+    id: 2,
+    image: 'https://img.zcool.cn/community/01bd695ad6cc1aa801204029d4e982.jpg@2o.jpg',
+    title: '铁板卤肉饭',
+    description: '秋哥湘式铁板炒米粉米饭',
+    price: 20,
+  },
+  {
+    id: 3,
+    image: 'https://img1.baidu.com/it/u=2279026368,1623499517&fm=253&fmt=auto&app=138&f=JPEG?w=667&h=500',
+    title: '香辣鸡排饭',
+    description: '红布坊鸡排饭',
+    price: 18.8,
+  },
+]
+const getDataSource=()=>{
+  return dataSource;
 }
 
 function App() {
-  //评价button响应
-  const [open, setOpen] = useState(false);
-  const showDrawer = () => {
-    setOpen(true);
-  };
-  const closeDrawer = () => {
-    setOpen(false);
-  };
+  const [dataSource,setDataSource]=useState([]);
 
-  const [open2, setOpen2] = useState(false);
-  const showDrawer2 = () => {
-    setOpen2(true);
-  };
-  const closeDrawer2 = () => {
-    setOpen2(false);
-  };  
+  useEffect(()=>{
+    const data=getDataSource();
+    setDataSource(data);
+  },[]);
 
-  const [open3, setOpen3] = useState(false);
-  const showDrawer3 = () => {
-    setOpen(true);
-  };
-  const closeDrawer3 = () => {
-    setOpen(false);
-  };
-
-  //控制drawer
-  const [isOpen,setIsOpen]=useState(false);
-  //已评价button响应
-  const [OpenText,setOpenText]=useState(false);
-  const [OpenRate,setOpenRate]=useState(false);
-  const [inputDisabled, setInputDisabled] = useState(false);
-  const handleButtonClick=()=>{
-    if(!OpenText&&!OpenRate){
-      setIsOpen(!isOpen);
-      setOpen(false);
+  const showComment=(index)=>{
+    const data=dataSource[index];
+    if(!data){
+      return;
     }
-    else{
-      setOpenText(true);
-      setOpenRate(true);
-      setInputDisabled(true);
+    data.show=!data.show;
+    setDataSource(dataSource.map(data=>({...data})));
+  }
+  const showButton=(index,rate)=>{
+    const data=dataSource[index];
+    if(!data[index].rate){
+      !data.show;
     }
   }
-
-  const [isOpen2,setIsOpen2]=useState(false);
-  const handleButtonClick2=()=>{
-    setIsOpen2(!isOpen2);
-    setOpen2(false);
+  const onChangeRate = (index, rate) => {
+    // 将 dataSource 拷贝到局部变量 data
+    const data = dataSource.map(d => ({ ...d }));
+    data[index].rate = rate;
+    setDataSource(data);
   }
 
-  const [isOpen3,setIsOpen3]=useState(false);
-  const handleButtonClick3=()=>{
-    setIsOpen3(!isOpen3);
-    setOpen3(false);
-  }
 
   return (
     <div className="App">
       <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
         <Layout>
           <Header style={headerStyle}>我的订单</Header>
-          <Layout hasSider>
-            <Content style={contentStyle}>
-{/* 第一个 */}
-              <Row gutter={8}>
-                <Col className="gutter-row" span={3}>
-                  <div>
-                    <Image
-                      width={150}
-                      src="https://preview.qiantucdn.com/58pic/40/86/03/88P58PICt6vC6E6DQZp46_PIC2018.jpg%21w1024_new_small"
-                    />
-                  </div>
-                </Col>
-                <Col className="gutter-row" span={3}>
-                  <div>
-                    <Title level={4}>肥牛米粉（大份） 1份</Title>
-                    <p>柳州螺狮粉</p>
-                    <p>￥25</p>
-                  </div>
-                </Col>
-                <Col className='gutter-row' span={3} offset={22}>
-                  <Button type="primary" onClick={showDrawer} style={isOpen ? ButtonCloseStyle : ButtonStyle}>
-                    {isOpen||(OpenText&&OpenRate) ? '已评价' : '评价'}
-                  </Button>
-                </Col>
-              </Row>
-              <Divider></Divider>
-              <Drawer placement="bottom" onClose={closeDrawer} open={open}>
-                <TextArea rows={4} maxLength={6} disabled={inputDisabled}/>
-                <br />
-                <Space direction="vertical" style={{ width: '100%' }}>
-                  <Rate />
-                </Space>
-                <Button type="primary" onClick={handleButtonClick} style={ButtonStyle}>
-                  提交
-                </Button>
-                <Button onClick={handleButtonClick} style={ButtonCloseStyle}>
-                  取消
-                </Button>
-              </Drawer>
-{/* 第二个 */}
-              <Row gutter={8}>
-                <Col className="gutter-row" span={3}>
-                  <div>
-                    <Image
-                      width={150}
-                      src="https://img.zcool.cn/community/01bd695ad6cc1aa801204029d4e982.jpg@2o.jpg"
-                    />
-                  </div>
-                </Col>
-                <Col className="gutter-row" span={3}>
-                  <div>
-                    <Title level={4}>铁板卤肉饭</Title>
-                    <p>秋哥湘式铁板炒米粉米饭</p>
-                    <p>￥20</p>
-                  </div>
-                </Col>
-                <Col className='gutter-row' span={3} offset={22}>
-                  <Button type="primary" onClick={showDrawer2} style={isOpen2 ? ButtonCloseStyle : ButtonStyle}>
-                    {isOpen2 ? '已评价' : '评价'}
-                  </Button>
-                </Col>
-              </Row>
-              <Divider></Divider>
-              <Drawer placement="bottom" onClose={closeDrawer2} open={open2}>
-                <TextArea rows={4} maxLength={6}/>
-                <br />
-                <Space direction="vertical" style={{ width: '100%' }}>
-                  <Rate />
-                </Space>
-                <Button type="primary" onClick={handleButtonClick2} style={ButtonStyle}>
-                  提交
-                </Button>
-                <Button onClick={handleButtonClick2} style={ButtonCloseStyle}>
-                  取消
-                </Button>
-              </Drawer>
-    {/* 第三个 */}
-              <Row gutter={8}>
-                <Col className="gutter-row" span={3}>
-                  <div>
-                    <Image
-                      width={150}
-                      src="https://img1.baidu.com/it/u=2279026368,1623499517&fm=253&fmt=auto&app=138&f=JPEG?w=667&h=500"
-                    />
-                  </div>
-                </Col>
-                <Col className="gutter-row" span={3}>
-                  <div>
-                    <Title level={4}>香辣鸡排饭</Title>
-                    <p>红布坊鸡排饭</p>
-                    <p>￥15</p>
-                  </div>
-                </Col>
-                <Col className='gutter-row' span={3} offset={22}>
-                  <Button type="primary" onClick={showDrawer3} style={isOpen3 ? ButtonCloseStyle : ButtonStyle}>
-                    {isOpen3 ? '已评价' : '评价'}
-                  </Button>
-                </Col>
-              </Row>
-              <Divider></Divider>
-              <Drawer placement="bottom" onClose={closeDrawer3} open={open3}>
-                <TextArea rows={4} maxLength={6}/>
-                <br />
-                <Space direction="vertical" style={{ width: '100%' }}>
-                  <Rate />
-                </Space>
-                <Button type="primary" onClick={handleButtonClick3} style={ButtonStyle}>
-                  提交
-                </Button>
-                <Button onClick={handleButtonClick3} style={ButtonCloseStyle}>
-                  取消
-                </Button>
-              </Drawer>
-            </Content>
-          </Layout>
+         {
+          dataSource.map((data,index)=>{
+            return(
+              <div key={index}>
+                <Content style={contentStyle}>
+                  <Row gutter={8}>
+                    <Col className="gutter-row" span={3}>
+                      <div>
+                      <Image
+                        width={150}
+                        src={data.image}className="img"
+                      />
+                      </div>
+                    </Col>
+                    <Col className="gutter-row" span={3}>
+                          <Title level={4}>{data.title}</Title>
+                          <p>{data.description}</p>
+                          <p>{data.price}</p>
+                    </Col>
+                    <Col className='gutter-row' span={3} offset={22}>
+                      <Button className='comment-button' style={data.rate?ButtonCloseStyle:ButtonStyle} onClick={() => showComment(index)}>
+                        { data.rate ? '已评价' : '评价' }
+                      </Button>
+                    </Col>
+                    {
+                      data.show?(
+                        <div>
+                          <TextArea autoSize="true" className='inputBoxBananaxxx' id='bananaInputxx' value={data.comment}></TextArea>
+                          <Rate
+                            className='rateBoxBananasss'
+                            id = 'rateBanana' 
+                            value={data.rate}
+                            onChange={(val) => { onChangeRate(index, val) }}
+                          />
+                        </div>
+                      ):null
+                    }
+                  </Row>
+                </Content>
+                
+               
+                {index!==data.length?<Divider/>:null}
+              </div>
+            )
+          })
+        }
         </Layout>
       </Space>
     </div>
   );
 }
-
+// 
 export default App;
